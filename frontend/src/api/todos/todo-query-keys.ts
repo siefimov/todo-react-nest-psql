3f36e47c49f5956413ef254303efd4ff0db7650b
Serhii <sergii.iefimov@gmail.com>
2025-05-20 19:22:49 +0300

    fr-todo-2: + useEditTodo

--- frontend/src/api/todos/todo-query-keys.ts
+++ frontend/src/api/todos/todo-query-keys.ts
@@ -1,5 +1,5 @@
 export const todoQueryKeys = {
   all: ['todos'] as const,
-  details: () => [...todoQueryKeys.all, 'dedatil'] as const,
+  details: () => [...todoQueryKeys.all, 'detail'] as const,
   detail: (id: string) => [...todoQueryKeys.details(), id] as const,
 };
